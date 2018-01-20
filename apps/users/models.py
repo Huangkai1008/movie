49025dbf46032ffc19b83b26da92828b631ce593
huangkai <18778335525@163.com>
2018-01-20 12:08:22 +0800

    添加xadmin并整合apps内容

--- apps/users/models.py
+++ apps/users/models.py
@@ -20,7 +20,7 @@ class UserProfile(models.Model):
         verbose_name = "用户信息"
         verbose_name_plural = verbose_name
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 
@@ -36,96 +36,10 @@ class UserLog(models.Model):
         verbose_name = "会员登录日志"
         verbose_name_plural = verbose_name
 
-    def __unicode__(self):
+    def __str__(self):
         return self.id
 
 
-class Tag(models.Model):
-    """
-    电影标签
-    """
-    name = models.CharField(max_length=50, unique=True, verbose_name="标签名")
-    add_time = models.DateTimeField(db_index=True, default=datetime.now, verbose_name="添加时间")
-
-    class Meta:
-        verbose_name = "电影标签"
-        verbose_name_plural = verbose_name
-
-    def __unicode__(self):
-        return self.name
-
-
-class Movie(models.Model):
-    """
-    电影模型
-    """
-    title = models.CharField(max_length=50, verbose_name="电影名")
-    url = models.URLField(max_length=255, unique=True, verbose_name="播放地址")
-    info = models.TextField(max_length=500, verbose_name="电影简介")
-    logo = models.ImageField(upload_to="image/%Y/%m", default="image/default.png",
-                             max_length=100, verbose_name="电影图")
-    star = models.SmallIntegerField(verbose_name="电影评星")
-    play_num = models.BigIntegerField(verbose_name="播放次数")
-    comment_num = models.BigIntegerField(verbose_name="评论次数")
-    tag_id = models.ForeignKey(Tag, verbose_name="电影所属标签", on_delete=models.CASCADE)
-    area = models.CharField(max_length=50, verbose_name="上映地区")
-    release_time = models.DateField(verbose_name="上映日期")
-    length = models.CharField(max_length=100, verbose_name="电影长度")
-    add_time = models.DateTimeField(db_index=True, default=datetime.now, verbose_name="添加时间")
-
-    class Meta:
-        verbose_name = "电影模型"
-        verbose_name_plural = verbose_name
-
-    def __unicode__(self):
-        return self.title
-
-
-class Preview(models.Model):
-    """
-    上映预告
-    """
-    title = models.CharField(max_length=50, unique=True, verbose_name="名称")
-    logo = models.ImageField(upload_to="image/%Y/%m", default="image/default.png",
-                             max_length=100, verbose_name="预告图")
-    add_time = models.DateTimeField(verbose_name="添加时间", db_index=True, default=datetime.now)
-
-    class Meta:
-        verbose_name = "上映预告"
-        verbose_name_plural = verbose_name
-
-    def __unicode__(self):
-        return self.title
-
-
-class Comment(models.Model):
-    """
-    评论
-    """
-    content = models.TextField(max_length=500, verbose_name="评论内容")
-    movie_id = models.ForeignKey(Movie, verbose_name="所属电影", on_delete=models.CASCADE)
-    user_id = models.ForeignKey(UserProfile, verbose_name="所属用户", on_delete=models.CASCADE)
-    add_time = models.DateTimeField(db_index=True, verbose_name="添加时间", default=datetime.now)
-
-    class Meta:
-        verbose_name = "电影评论"
-        verbose_name_plural = verbose_name
-
-    def __unicode__(self):
-        return self.id
-
-
-class MovieLike(models.Model):
-    """
-    电影收藏
-    """
-    movie_id = models.ForeignKey(Movie, verbose_name="所属电影", on_delete=models.CASCADE)
-    user_id = models.ForeignKey(UserProfile, verbose_name="所属用户", on_delete=models.CASCADE)
-    add_time = models.DateTimeField(db_index=True, verbose_name="添加时间", default=datetime.now)
-
-    class Meta:
-        verbose_name = "电影收藏"
-        verbose_name_plural = verbose_name
 
 
 
